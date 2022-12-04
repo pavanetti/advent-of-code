bdf48af636e49656c43fdf5a8c0c93ef6057595c
Carlos Pavanetti <carlos.pavanetti@buser.com.br>
2022-12-04 15:34:57 -0300

    Add solution for challenge 202203/1

---
+++ 2022/ch03_1.clj
@@ -0,0 +1,47 @@
+(require '[clojure.set :as set])
+
+(defn repeated-items
+  [fst-seq snd-seq]
+  (apply list
+    (set/intersection
+      (set fst-seq)
+      (set snd-seq))))
+
+(defn wrong-item
+  [seq]
+  (let [size (count seq)
+        middle (/ size 2)]
+    (first
+      (apply repeated-items
+        (split-at middle seq)))))
+
+(defn item-priority
+  [item-as-char]
+  (let [num-value (int item-as-char)
+        lower-a (int \a)
+        upper-a (int \A)]
+    (cond
+      (>= num-value lower-a)
+        (inc (- num-value lower-a))
+      :else
+        (+ 27 (- num-value upper-a)))))
+
+(defn priority-sum-of-wrong-items
+  [rucksacks]
+  (apply +
+    (map
+      (fn [sack] (item-priority (wrong-item sack)))
+      rucksacks)))
+
+(defn rucksacks-from-file
+  [file-name]
+    (with-open [rdr (BufferedReader. (FileReader. file-name))]
+      (doall (line-seq rdr))))
+
+(defn find-answer
+  [args]
+  (priority-sum-of-wrong-items
+    (rucksacks-from-file
+      (or (first args) "ch03_1.txt"))))
+
+(defn -main [] (find-answer *command-line-args*))
